3d405e246e22b3b362357ab3f83eae97d908ebd0
Jordy D'Hoker <jordydhoker@gmail.com>
2019-09-21 18:25:52 +0200

    made code fancy

--- bot.js
+++ bot.js
@@ -1,65 +1,65 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require('fs')
+const fs = require("fs");
+
 let isReady = true;
 
-client.on("message", message => {
-    if (message.author.username === "MAHOE") {
-        return;
-    }
-    if (message.author.id == 113733053150527497) {
-        //fuck kabnaar
-        message.reply('zwijg is');
-        return;
-    }
-    if (
-        message.content.slice(0, 1) !== "%" ||
-        (!isReady && !checkChannel(message))
-        ) {
-        return;
-    }
+client.on("message", async message => {
+    const state = {
+        message,
+        sound: message.content.slice(1),
+        voiceChannel: message.member.voiceChannel
+    };
 
-    const sound = message.content.slice(1);
+    if (!canRun(state)) return;
 
-    if (fs.existsSync('./sounds/' + sound + '.mp3')) {
-        const voiceChannel = message.member.voiceChannel;
-        console.log('Playing ' + sound + ' for ' + message.author.username + ' in channel ' + message.member.voiceChannel.name);
-        joinVoice(voiceChannel, sound);
-    }
-    else {
-        message.reply(sound + ' does not exist');
-        console.log(message.author.username + ' tried playing ' + sound);
-    }
-
+    isReady = false;
+    await run(state);
+    isReady = true;
 });
 
-
-function checkChannel(message) {
-    if (!message.member.voiceChannel) {
-        message.reply('ga is in ne voicechannel bloten');
+function canRun({ message, ...state }) {
+    //bot wont run if this returns false
+    if (
+        message.author.id === 509464429037879323 || //if is mahoebot
+        message.content.slice(0, 1) !== "%" || //if message does not begin with '%'
+        !isReady
+    ) {
         return false;
     }
+
+    if (!state.voiceChannel) {
+        message.reply("ga is in ne voicechannel bloten");
+        return false;
+    }
+
+    if (!fs.existsSync("./sounds/" + state.sound + ".mp3")) {
+        message.reply(state.sound + " does not exist");
+        return false;
+    }
+
+    if (message.author.id == 113733053150527497) {
+        //fuck kabnaar
+        message.reply("zwijg is");
+        return false;
+    }
+
     return true;
 }
 
-function joinVoice(voiceChannel, sound) {
-    isReady = false;
-    voiceChannel.join().then(connection => {
-        setTimeout(play, 1000, connection, sound, voiceChannel);
-    }).catch(err => console.log(err));
+async function run(state) {
+    try {
+        const connection = await state.voiceChannel.join();
+        connection
+            .playFile("./sounds/" + state.sound + ".mp3")
+            .on("end", () => setTimeout(() => state.voiceChannel.leave(), 1000));
+    } catch (err) {
+        state.message.reply("Zeg tegen <@119523208192983045> da er een error is");
+        console.log(err);
+    }
 }
 
-function play(connection, sound, voiceChannel) {
-    const dispatcher = connection.playFile('./sounds/' + sound + '.mp3');
-    dispatcher.on("end", end => setTimeout(leaveVoice, 1000, voiceChannel));
-}
-
-function leaveVoice(voiceChannel) {
-    voiceChannel.leave();
-    isReady = true;
-}
-
-const {token} = require('./auth.json');
+const { token } = require("./auth.json");
 client.login(token);
 
-console.log('Running MAHOE')
+console.log("Running MAHOE");
